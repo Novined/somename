9dd753685377bd895338de900430b53c9966ae25
Anykey228 <cahcahbi432@yandex.ru>
2016-10-15 04:14:25 +0300

    'shit'

---
+++ Assets/PC2D/Scripts/Sanya/Copy1.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Copy1 : MonoBehaviour
+{
+
+ float speed;
+  Vector3 target;
+  Vector3 start;
+  Vector3 pos;
+
+    void  Start()
+    {
+        start = transform.position;
+        pos = transform.position;
+    }
+
+    void Update()
+    {
+        if (Input.GetButton("Fire2"))
+        {
+            Vector3 temp = Input.mousePosition;
+            temp.z = 10f; // Set this to be the distance you want the object to be placed in front of the camera.
+            this.transform.position = Camera.main.ScreenToWorldPoint(temp);
+        }
+    }
+}
